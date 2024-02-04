367361ac520e8a8a4a7cae190edcd31bb1b1e7f5
izzy2fancy <121840901+izzy2fancy@users.noreply.github.com>
2024-02-04 10:28:54 -0500

    Update not working link

--- patcher.sh
+++ patcher.sh
@@ -45,7 +45,7 @@ if ! [ -f /data/data/com.termux/files/usr/bin/apkmod ]; then
 fi
 
 #download the patch data
-svn export https://github.com/AM2R-Community-Developers/AM2R-Autopatcher-Android/trunk/data
+svn export https://github.com/AM2R-Community-Developers/AM2R-Autopatcher-Android/tree/main/data
 
 #check if termux-storage has been setup
 if ! [ -d ~/storage ]; then
