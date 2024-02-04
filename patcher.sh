6de0eb7138f5bc3d7f8e33320d177b858027316a
izzy2fancy <121840901+izzy2fancy@users.noreply.github.com>
2024-02-04 11:39:28 -0500

    Update link and make building alittle more automatic

--- patcher.sh
+++ patcher.sh
@@ -35,7 +35,7 @@ echo ""
 echo "-------------------------------------------"
 
 #install dependencies
-pkg install -y subversion zip unzip xdelta3
+pkg install -y subversion zip unzip xdelta3 libuuid wget
 
 #check if apkmod is instaled, if not install it. I only use this for signing 'cause it's the only way I found this to work
 if ! [ -f /data/data/com.termux/files/usr/bin/apkmod ]; then
@@ -45,7 +45,7 @@ if ! [ -f /data/data/com.termux/files/usr/bin/apkmod ]; then
 fi
 
 #download the patch data
-svn export https://github.com/izzy2fancy/AM2R-Autopatcher-Android/tree/main/data
+svn export https://github.com/izzy2fancy/AM2R-Autopatcher-Android/raw/main/data/droid.xdelta
 
 #check if termux-storage has been setup
 if ! [ -d ~/storage ]; then
